60296a8a3610c4f526282a7b7fe5a139c39c65a2
Asif Shamal <asifshamal51@outlook.com>
2023-09-16 21:13:46 +0430

    added mouse events and buttons functions

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 const canvas = document.getElementById('canvas')
+
 const ctx = canvas.getContext('2d')
+
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+
+
 
 let size =10
 let color = 'black'
+let x
+let y
+let isPressed = false
+
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+    x = e.offsetX
+    y = e.offsetY
+
+    drawCircle(x,y)
+
+
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2,y2)
+        drawLine(x,y,x2,y2)
+        x = x2 
+        y = y2
+    }
+
+})
+
 
 function drawCircle(x,y) {
     ctx.beginPath()
@@ -21,5 +67,31 @@ function drawLine(x1,y1,x2,y2){
     ctx.stroke()
 }
 
-drawCircle(100,300)
-drawLine(200,200,500,500)
+
+function updateSizeOnScreen() {
+    sizeEL.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
